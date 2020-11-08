b683f72889e30762f181c978922a14431f85fa30
Seb <titi@toto.fr>
2020-11-08 21:18:17 +0100

    refactor(DS18B20): utilise le fichier temperature plus simple à parser

--- ds18b20/read-sensor.sh
+++ ds18b20/read-sensor.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-SENSOR_PATH=$1
-RETRY=0
+SENSOR_TEMPERATURE_PATH=$1
 
-while [ "$CRC" != "YES" ]; do
-    RAW_VALUE=$(cat $SENSOR_PATH)
-    CRC=$(echo $RAW_VALUE | head -n 1)
-    CRC=${CRC:36:3}
-    #echo "CRC=${CRC}"
-
-    let "RETRY+=1"
-    if [ $RETRY -ge 100 ] 
-    then
-        echo "Impossible de lire une température sur la sonde $SENSOR_PATH."
-        exit 1
-    fi
-done
-
-TEMPERATURE=$(echo -e "$RAW_VALUE" | sed '2q;d')
-TEMPERATURE=${TEMPERATURE:29:2}.${TEMPERATURE:31:3}
-#echo "TEMPERATURE=${TEMPERATURE}°C"
+TEMPERATURE=$(cat $SENSOR_TEMPERATURE_PATH)
+TEMPERATURE=${TEMPERATURE:0:2}.${TEMPERATURE:2:3}
 echo ${TEMPERATURE}
