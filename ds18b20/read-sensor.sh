d85fedcc3755820931ef90c9e42400556359c031
Seb <titi@toto.fr>
2020-11-08 20:05:47 +0100

    fix(DS18B20): gère les problèmes de lecture sur une sonde

--- ds18b20/read-sensor.sh
+++ ds18b20/read-sensor.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 SENSOR_PATH=$1
+RETRY=0
 
 while [ "$CRC" != "YES" ]; do
     RAW_VALUE=$(cat $SENSOR_PATH)
     CRC=$(echo $RAW_VALUE | head -n 1)
     CRC=${CRC:36:3}
     #echo "CRC=${CRC}"
+
+    let "RETRY+=1"
+    if [ $RETRY -ge 100 ] 
+    then
+        echo "Impossible de lire une température sur la sonde $SENSOR_PATH."
+        exit 1
+    fi
 done
 
 TEMPERATURE=$(echo -e "$RAW_VALUE" | sed '2q;d')
