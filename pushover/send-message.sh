e174ae64b669295c1c123407f2d750b0a93020da
Seb <test@test>
2020-11-23 22:42:44 +0000

    feat(pushover): gère les urgences

--- pushover/send-message.sh
+++ pushover/send-message.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-APP_TOKEN=
-USER_TOKEN=
 TITLE=$1
 MESSAGE=$2
 PRIORITY=$3
+RETRY=$4
+EXPIRE=$5
 
 wget https://api.pushover.net/1/messages.json \
-    --post-data="token=$APP_TOKEN&user=$USER_TOKEN&message=$MESSAGE&title=$TITLE&priority=$PRIORITY" \
+    --post-data="token=$APP_TOKEN&user=$USER_TOKEN&message=$MESSAGE&title=$TITLE&priority=$PRIORITY&retry=$RETRY&expire=$EXPIRE" \
     -qO- > /dev/null 2>&1 &
