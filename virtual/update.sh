d6b056f76817866203d05b8b6396f4e209966597
Seb <test@test>
2020-11-23 22:49:18 +0000

    virtual

--- virtual/update.sh
+++ virtual/update.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-curl \
-    --location \
-    --request GET "http://$JEEDOM_HOST/core/api/jeeApi.php?plugin=virtual&apikey=$JEEDOM_API_KEY&type=virtual&id=$1&value=$2"
+echo $JEEDOM_HOST
+
+echo "http://${JEEDOM_HOST}/core/api/jeeApi.php?plugin=virtual&apikey=${JEEDOM_API_KEY}&type=virtual&id=$1&value=$2"
+
+curl --location --request GET "http://${JEEDOM_HOST}/core/api/jeeApi.php?plugin=virtual&apikey=${JEEDOM_API_KEY}&type=virtual&id=$1&value=$2"
