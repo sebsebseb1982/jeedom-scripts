85f9da3ff80497c7b8ff02125f7bbdbc6ed89994
Seb <titi@toto.fr>
2021-07-17 15:37:20 +0200

    ups

--- ups/status.sh
+++ ups/status.sh
@@ -2,8 +2,9 @@
 SYNOLOGY_HOST=192.168.1.169
 SYNOLOGY_PORT=5000
 
-LOGIN=$1
-PASSWORD=$2
+REQUESTED_VALUE=$1
+LOGIN=$2
+PASSWORD=$3
 
 AUTH_PATH=$(curl \
     --location \
@@ -29,7 +30,7 @@ UPS_PATH=$(curl \
 curl \
     --location \
     -s \
-    --request GET "$SYNOLOGY_HOST:$SYNOLOGY_PORT/webapi/$UPS_PATH?api=SYNO.Core.ExternalDevice.UPS&method=get&version=1&_sid=$SID" | jq '.data'
+    --request GET "$SYNOLOGY_HOST:$SYNOLOGY_PORT/webapi/$UPS_PATH?api=SYNO.Core.ExternalDevice.UPS&method=get&version=1&_sid=$SID" | jq --arg v "$REQUESTED_VALUE" '.data[$v]'
 
 curl \
     --location \
